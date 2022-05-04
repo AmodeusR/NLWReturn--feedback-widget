8e52b569f39d3fc9f60547960b12a47ed9ba8b26
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-03 22:27:07 -0300

    added expanded colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E5",
+          600: "#996DFF"
+        }
+      }
+    },
   },
   plugins: [],
 }
