f84833eb7155eb01070126df0269e0d185b197b0
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-06 00:15:09 -0300

    Update CloseButton.tsx

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,12 +1,17 @@
 import { Popover } from "@headlessui/react"
 import { X } from "phosphor-react"
 
-const CloseButton = () => {
+interface Props {
+  className?: string
+}
+
+const CloseButton = ({ className }: Props) => {
+
   return (
-    <Popover.Button className="hover:text-[crimson] transition-colors" title="Fechar formulário de feedback">
+    <Popover.Button className={`hover:text-zinc-100 text-zinc-400 transition-colors absolute right-5 top-5.5 ${!!className && className}`} title="Fechar formulário de feedback">
       <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
-    )
+    );
 }
 
 export default CloseButton
