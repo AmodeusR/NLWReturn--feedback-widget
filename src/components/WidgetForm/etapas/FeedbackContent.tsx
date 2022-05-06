a5b2028556a3cb3e391c8ed2d1e2a4919e9a4e3f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-06 00:13:46 -0300

    Created FeedbackContent component

---
+++ src/components/WidgetForm/etapas/FeedbackContent.tsx
@@ -0,0 +1,77 @@
+import { FormEvent, useState } from "react";
+
+import CloseButton from "../../CloseButton";
+import ReturnButton from "../../ReturnButton";
+import ScreenshotButton from "../ScreenshotButton";
+
+import { FeedbackType, feedbackTypes } from "..";
+
+interface Props {
+  feedbackType: FeedbackType;
+  handleFeedbackRestart: () => void;
+  setFeedbackSent: (type: boolean) => void;
+}
+
+const FeedbackContent = ({ feedbackType, handleFeedbackRestart, setFeedbackSent }: Props) => {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [feedbackComment, setFeedbackComment] = useState("");
+
+  const handleSubmission = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log({
+      feedbackComment,
+      screenshot
+    });
+
+    setFeedbackSent(true);    
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="title text-xl flex items-center self-center">
+        <ReturnButton handleFeedbackRestart={handleFeedbackRestart} />
+        <h2 className="flex items-center gap-1.5 font-medium">
+          <img
+            className="h-6 w-6"
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+          />
+          {feedbackTypeInfo.title}
+        </h2>
+        <CloseButton />
+      </div>
+
+      <form className="flex flex-col gap-2" onSubmit={handleSubmission}>
+        <textarea
+          placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+          className="
+            md:min-w-[304px] min-h-[112px] text-sm rounded bg-transparent px-2 py-1
+          border-zinc-600 resize-none leading-5
+            focus:outline-none transition-colors focus:ring-brand-500 
+            scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent
+          "
+          onChange={(e) => setFeedbackComment(e.target.value)}
+          value={feedbackComment}
+        ></textarea>
+        <div className="buttons flex gap-2 w-full">
+          <ScreenshotButton setScreenshot={setScreenshot} screenshot={screenshot} />
+          <button
+            disabled={feedbackComment.length === 0}
+            type="submit"
+            className="
+              bg-brand-500 rounded p-2 font-medium hover:bg-brand-300 transition-colors flex-1
+              focus:outline-none focus:ring-brand-500 focus:ring-offset-2 focus:ring-2 ring-offset-zinc-900
+              disabled:opacity-50 disabled:hover:bg-brand-500
+          ">
+            Enviar feedback
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FeedbackContent;
