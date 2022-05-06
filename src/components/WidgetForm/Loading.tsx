e3d66cd9a5cf06e8c025498aedf6b1a687067c9c
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-06 00:14:08 -0300

    Create Loading.tsx

---
+++ src/components/WidgetForm/Loading.tsx
@@ -0,0 +1,9 @@
+import { CircleNotch } from "phosphor-react"
+
+const Loading = () => {
+  return (
+    <CircleNotch weight="bold" className="h-6 w-6 animate-spin" />
+  )
+}
+
+export default Loading
