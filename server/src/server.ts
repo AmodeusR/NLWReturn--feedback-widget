e2148d7407d43dba8f834081cfeef132cf81923f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-06 12:15:06 -0300

    added mail notification

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "0e3a30c118d8ed",
+    pass: "5f725f94bd4ac6"
+  }
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +25,16 @@ app.post("/feedbacks", async (req, res) => {
       screenshot
     }
   });
+
+  await transport.sendMail({
+    from: "Equipe Feedback <devteste@feedget.com>",
+    to: "Amodeus R. <RichardGSullivan@hotmail.com>",
+    subject: "Você recebeu um novo Feedback",
+    html: [
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Commentário do feedback: ${comment}</p>`,
+    ].join("\n")
+  })
   
   return res.status(201).json({ data: feedback });
 })
